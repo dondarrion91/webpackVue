d3f6704c0e692f78183e4dbfd1d850c00fc3933b
dondarrion91 <julisanchez1991@gmail.com>
2020-08-29 01:14:51 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const {
+    VueLoaderPlugin
+} = require('vue-loader');
+
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new htmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
